ce5fd84210b4c2fe02d73fdaef5b254d02d7a866
unrar <joszaynka@gmail.com>
2017-02-28 00:15:46 +0100

    Fixes #2 - Added a command to authenticate yourself! ✨

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 *NowOnLastFM* is a Python plug-in for Hexchat. Using the Hexchat wrapper for Python and the [pylast](https://github.com/pylast/pylast) fabulous library of tools to plug into the LastFM (and other similar services) API in a very easy way. 💖
 
-Once you've configured and authorized the plugin for your LastFM account, you'll be the coolest guy in the room. Whenever a cool song pops up, let everyone know you're listening to it by typing:
+Once you've configured and authorized the plugin for your LastFM account (running `/fmlogin <username>`), you'll be the coolest guy in the room. Whenever a cool song pops up, let everyone know you're listening to it by typing:
 
     /listening
 
@@ -20,24 +20,19 @@ If you can't figure it out, check out [pylast](https://github.com/pylast/pylast)
 
 Other than that, the Hexchat python library should already be installed and ready to use.
 
-## Long guide for the easiest configuration ever
-
-For now, you need to manually change your username on the code - in further releases I'll try to make the authentication process a bit more separated, as in another command.
+## Logging in, simpler than ever 🔐
 
 You just need to download (as ZIP) or clone the plugin into whichever folder you want:
 
     $ git clone https://github.com/unrar/nowonlastfm
 
+As of `v1.2.0`, you don't need to touch the code anymore! All you need to do to authenticate yourself, is run the `fmlogin` command:
 
-And then open the source file, `nowonlastfm.py`, with your **plain** text editor of choice. Find `USERNAME =` (it's around line 23) and, if your username is `metalhead` you should replace it for:
+    /fmlogin <username>
 
-```python
-USERNAME = "metalhead"
-```
+That should be it! It needs to be your username, because when you run this command you will be prompted to your browser in order to, being logged into that username, give permission for the plugin to access your account. Don't worry, it only allows to see your profile and your scrobbles; in order to change anything, your password would be required to use the API.
 
-That should be it! It needs to be your username, because when you first load the plugin into Hexchat you will be prompted to your browser in order to, being logged into that username, give permission for the plugin to access your account. Don't worry, it only allows to see your profile and your scrobbles; in order to change anything, your password would be required to use the API.
-
-So that's pretty much it! Now just open Hexchat, and depending on your OS, find something like `File > Load plugin or script...` and browse to the folder named `nowonlastfm` where you downloaded and edited the source, `nowonlastfm.py`. 
+So that's pretty much it! Now just open Hexchat, and depending on your OS, find something like `File > Load plugin or script...` and browse to the folder named `nowonlastfm` where you downloaded the source, `nowonlastfm.py`. 
 
 If you experience any problems, please get in touch with me! 💌
 
--- nowonlastfm.py
+++ nowonlastfm.py
@@ -11,53 +11,76 @@ import os
 import time
 
 __module_name__ = "NowOnLastFM"
-__module_version__ = "1.1.2"
+__module_version__ = "1.2.0"
 __module_description__ = "Lets everyone know what you're scrobbling on LastFM"
 
 # These API keys should remain unchanged; you only need to modify the USERNAME
 API_KEY = "f7ecb9e71532280168393c165e7748a6"
 API_SECRET = "98c2c83a6fec31fb47a947b4a8cb6a3e"
 SESSION_KEY_FILE = os.path.join(os.path.expanduser("~"), ".session_key")
-
-# Change this with your username.
-USERNAME = "joszaynka" 
+USERNAME_FILE = os.path.join(os.path.expanduser("~"), ".nolfm_user")
 
 # Connect to the LastFM API via my application
 network = pylast.LastFMNetwork(API_KEY, API_SECRET)
 
-# On the first use, the user needs to authorize this application onto their account
+# This command lets the user log into their account, if they haven't already
+def fmlogin(word, word_eol, userdata):
+  # Need one argument (the username)
+  if not len(word) > 1:
+    hexchat.command("help fmlogin")
+    return hexchat.EAT_ALL
+  else:
 
-if not os.path.exists(SESSION_KEY_FILE):
-  skg = pylast.SessionKeyGenerator(network)
-  url = skg.get_web_auth_url()
+    # If there's no session key
+    if not os.path.exists(SESSION_KEY_FILE):
+      skg = pylast.SessionKeyGenerator(network)
+      url = skg.get_web_auth_url()
 
-  hexchat.prnt("Please authorise the scrobbler: %s" % url)
-  import webbrowser
-  webbrowser.open(url)
+      hexchat.prnt("Please authorise the scrobbler: %s" % url)
+      import webbrowser
+      webbrowser.open(url)
 
-  while True:
-    try:
-      session_key = skg.get_web_auth_session_key(url)
-      fp = open(SESSION_KEY_FILE, "w")
-      fp.write(session_key)
-      fp.close()
-      break
-    except pylast.WSError:
-      time.sleep(1)
-else:
-  session_key = open(SESSION_KEY_FILE).read()
+      while True:
+        try:
+          session_key = skg.get_web_auth_session_key(url)
+          fp = open(SESSION_KEY_FILE, "w")
+          fp.write(session_key)
+          fp.close()
+          break
+        except pylast.WSError:
+          time.sleep(1)
+    else:
+      session_key = open(SESSION_KEY_FILE).read()
 
-# Now the user's been identified, so we have a session key
-network.session_key = session_key
-user = network.get_user(USERNAME)
+    # Whether's a username file already or not, we replace it
+    fp = open(USERNAME_FILE, "w")
+    fp.write(word[1])
+    fp.close()
+
+    # Now we have a key and a username
+    network.session_key = session_key
+    return hexchat.EAT_ALL
 
 # Now we just need to get the song
 def get_song(word, word_eol, userdata):
+  # Check if the user is logged in
+  if (not os.path.exists(SESSION_KEY_FILE)) or (not os.path.exists(USERNAME_FILE)):
+    hexchat.prnt(">> You need to log into your lastfm account: /fmlogin <username>")
+    return hexchat.EAT_ALL
+
+  # The user is logged in, so proceed
   try:
+    # Get the username from the file
+    fp = open (USERNAME_FILE, "r")
+    username = fp.read()
+    fp.close()
+
+    # And find out what he's listening to
+    user = network.get_user(username)
     track = user.get_now_playing()
     hexchat.command('me is listening to ' + str(track.title) + ' by ' + str(track.artist) + '.')
   except:
-    hexchat.prnt("Ground control to Major Tom, something's gone wrong! Are you sure you're scrobbling to lastfm?")
+    hexchat.prnt(">> Something's gone wrong! Are you sure you're scrobbling to lastfm?")
   return hexchat.EAT_ALL
 
 # We also want to make it unload nicely
@@ -66,5 +89,6 @@ def unload_mod(userdata):
 
 # Now we just need to hook this to Hexchat.
 hexchat.hook_command('listening', get_song, help='Use /listening with no arguments')
+hexchat.hook_command('fmlogin', fmlogin, help="/fmlogin <username>, needed to authenticate yourself.")
 hexchat.hook_unload(unload_mod)
 print(__module_name__ + ' version ' + __module_version__ + ' loaded. Start scrobbling and type /listening on a channel :)')
